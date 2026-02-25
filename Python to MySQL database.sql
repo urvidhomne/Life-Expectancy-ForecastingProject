ee32b8ee62f0ef86a1249c85e4d449d8b24ceb1a
Urvi Dhomne <58173803+urvidhomne@users.noreply.github.com>
2026-02-25 14:13:15 -0500

    Delete Python to MySQL database.sql

--- Python to MySQL database.sql
+++
@@ -1,6 +0,0 @@
-CREATE USER 'urvi_user'@'localhost' IDENTIFIED BY 'serverurd';
-
-CREATE DATABASE life_expectancy_db;
-
-GRANT ALL PRIVILEGES ON life_expectancy_db.* TO 'urvi_user'@'localhost';
-FLUSH PRIVILEGES;
